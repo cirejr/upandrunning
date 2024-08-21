f02b9e35d53ed70872e31e71b6f05b43eebaf23d
cirejr <juniorcireba@gmail.com>
2024-08-21 13:54:18 +0000

    fx: responsivity in header

--- components/global/header.tsx
+++ components/global/header.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { ModeToggle } from "./mode-toggle";
 import { links } from "@/config/site";
 import { Button } from "../ui/button";
+import MobileSideMenu from "./mobile-side-menu";
 
 export default function Header({
   isSticky,
@@ -28,11 +29,11 @@ export default function Header({
         UPnRunning
       </Link>
 
-      <nav className='flex items-center gap-4 sm:gap-6'>
+      <nav className='hidden items-center gap-4 sm:gap-6 md:flex'>
         {links.map((link, index) => (
           <Link
             key={index}
-            className='text-sm font-medium hover:underline underline-offset-4'
+            className='text-sm font-medium underline-offset-4 hover:underline'
             href={link.href}
           >
             {link.title}
@@ -40,9 +41,12 @@ export default function Header({
         ))}
       </nav>
       <div className='flex items-center gap-4'>
-        <Button variant='outline'>Get Started</Button>
+        <Button className='hidden md:block' variant='outline'>
+          Get Started
+        </Button>
         <ModeToggle />
       </div>
+      <MobileSideMenu isGlassy />
     </header>
   );
 }
---
+++ components/global/mobile-side-menu.tsx
@@ -0,0 +1,46 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { links } from "@/config/site";
+import Link from "next/link";
+import { HiMenuAlt3 } from "react-icons/hi";
+import { ModeToggle } from "./mode-toggle";
+
+export default function MobileSideMenu({ isGlassy }: { isGlassy?: boolean }) {
+  return (
+    <div className='block md:hidden'>
+      <Sheet>
+        <SheetTrigger asChild>
+          <HiMenuAlt3 className='h-6 w-6 text-black dark:text-white' />
+        </SheetTrigger>
+        <SheetContent
+          side='left'
+          className={` ${
+            isGlassy && "bg-transparent backdrop-blur-md backdrop-filter"
+          }`}
+        >
+          <SheetHeader>
+            <Link className='flex text-3xl font-bold tracking-tighter' href='/'>
+              UPnRunning
+            </Link>
+          </SheetHeader>
+          <nav className='mt-7 flex flex-col items-start gap-4 sm:gap-6'>
+            {links.map((link, index) => (
+              <Link
+                key={index}
+                className='font-medium underline-offset-4 hover:underline'
+                href={link.href}
+              >
+                {link.title}
+              </Link>
+            ))}
+          </nav>
+        </SheetContent>
+      </Sheet>
+    </div>
+  );
+}
